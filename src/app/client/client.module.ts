4d7acbc1003a37c1f0790f6d3eea149e39a99f39
a31710 <materkaman576@gmail.com>
2021-10-31 19:06:30 +0700

    update client + admin Form

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientComponent } from './client.component';
 import { ClientRoutingModule } from './client-routing-module';
-import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+
 
 @NgModule({
   imports: [
     CommonModule,
     ClientRoutingModule,
   ],
-  declarations: [ClientComponent]
+  declarations: [ClientComponent,HeaderComponent,SideBarComponent]
 })
 export class ClientModule { }
